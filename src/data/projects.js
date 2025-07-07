acba243c2b4eddaadc2eabad15d721e9c72a19a8
6127613876-1 <gokulpriyan781@gmail.com>
2025-07-07 18:51:12 +0530

    3

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,15 @@
 const projects = [
-  {
-    id: "1",
-    title: " Decentrailzed crowdfunding",
-    description:
-      " A decentralized crowdfunding platform enabling transparent and secure fundraising through smart contracts.Streamlined campaign creation processes, empowering users with ownership and real-time transaction visibility",
-    image: "/EL/Intro.png",
-    technologies: ["React", "Node.js", "Tailwindcss", "Thirdweb",],
-    liveUrl: "https://example.com",
-    githubUrl: "https://github.com/example",
-    category: "Full Stack Development"
-  },
+  // {
+  //   id: "1",
+  //   title: " Decentrailzed crowdfunding",
+  //   description:
+  //     " A decentralized crowdfunding platform enabling transparent and secure fundraising through smart contracts.Streamlined campaign creation processes, empowering users with ownership and real-time transaction visibility",
+  //   image: "/EL/Intro.png",
+  //   technologies: ["React", "Node.js", "Tailwindcss", "Thirdweb",],
+  //   liveUrl: "https://example.com",
+  //   githubUrl: "https://github.com/example",
+  //   category: "Full Stack Development"
+  // },
   {
     id: "2",
     title: " AI-Powered Course Learning System",
@@ -17,8 +17,8 @@ const projects = [
       "Developed a YouTube-based learning platform with personalized course paths and video recommendations using Gemini Pro.",
     image: "/EL/Intro.png",
     technologies: ["React.js", "Firebase","Gemini Pro","Youtube API","Flask"],
-    liveUrl: "https://example.com",
-    githubUrl: "https://github.com/example",
+    liveUrl: "https://www.e-learning.gokulcb.com/",
+    githubUrl: "https://github.com/6127613876-1/E-learning",
     category: "AI/ML"
   },
   {
@@ -26,7 +26,7 @@ const projects = [
     title: " OTT Platform",
     description:
       "An OTT platform featuring seamless video streaming, user authentication, and personalized content browsing experience.",
-    image: "https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?w=800&h=600&fit=crop",
+    image: "/OTT/movie.png",
     technologies: ["React.js", "Firebase", "TailwindcSS"],
     liveUrl: "https://example.com",
     githubUrl: "https://github.com/example",
@@ -39,7 +39,7 @@ const projects = [
     image: "/Fake/Fake.png",
     technologies: ["Python", "TensorFlow", "Flask", "NLP"],
     liveUrl: "https://example.com",
-    githubUrl: "https://github.com/example",
+    githubUrl: "https://github.com/6127613876-1/Fake-news-Detection",
     category: "AI/ML"
   },
   {
