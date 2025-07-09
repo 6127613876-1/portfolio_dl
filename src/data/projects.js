eed4e2310e2d047918d9485d6ffb8f4ff0cb865f
6127613876-1 <gokulpriyan781@gmail.com>
2025-07-09 19:00:34 +0530

    5

--- src/data/projects.js
+++ src/data/projects.js
@@ -51,6 +51,26 @@ const projects = [
     liveUrl: "https://example.com",
     githubUrl: "https://github.com/example",
     category: "Web Development"
+  },
+  {
+    id: "6",
+    title: "SBW Trust",
+    description:"SBW Trust, a non-profit organization dedicated to education and empowerment.",
+    image: "/Trust/trust.png",
+    technologies: ["Html", "Bootstrap", "js"],
+    liveUrl: "https://www.sbwtrust.org/",
+    githubUrl: "https://github.com/iamVinothKumar27/sbwt",
+    category: "Web Development"
+  },
+  {
+    id: "7",
+    title: "TCE HR CONCLAVE",
+    description:"Developed and deployed the official event site for the “National Innovation Conclave 2025” using ReactJS, Tailwind CSS, and Firebase, with features including speaker bios, schedules, and registration",
+    image: "/Conclave/tce_cc.png",
+    technologies: ["React.js", "Firebase", "TailwindcSS"],
+    liveUrl: "https://conclave.gokulcb.com/",
+    githubUrl: "https://github.com/6127613876-1/conclave-app",
+    category: "Full Stack Development"
   }
 ];
 
